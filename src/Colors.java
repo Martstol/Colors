43dfec11597bdda6d9421b23f6f096922c7b0ac4
KamiKagutsuchi <kamikagutsuchi@gmail.com>
2014-03-08 20:44:03 +0100

    Optimized(?) code and abstracted it
    Just extend PixelDiff, override calcPos and pass the new class as an argument to the createImage method to calculate the best position for the next pixel differently

--- src/Colors.java
+++ src/Colors.java
@@ -5,22 +5,19 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 /*
- * Unoptimized Java implementation of this: http://codegolf.stackexchange.com/questions/22144/images-with-all-colors/22326#22326
+ * Optimized Java implementation of this: http://codegolf.stackexchange.com/questions/22144/images-with-all-colors/22326#22326
  */
 
 public class Colors {
 	
 	// Algorithm settings
-	public static final boolean AVERAGE = false;
 	public static final int NUMCOLORS = 32; // NUMCOLORS^3 has to be equal to WIDTH*HEIGHT
 	public static final int WIDTH = 256;
 	public static final int HEIGHT = 128;
@@ -28,7 +25,7 @@ public class Colors {
 	public static final int STARTY = 64;
 	
 	public static void main(String[] args) {
-		final BufferedImage img = createImage();
+		final BufferedImage img = createImage(new MinimizeMinDiff(), WIDTH, HEIGHT, NUMCOLORS, STARTX, STARTY);
 		
 		try {
 			ImageIO.write(img, "png", new File("output.png"));
@@ -39,33 +36,38 @@ public class Colors {
 		final JFrame frame = new JFrame();
 		frame.setLayout(new BorderLayout());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		
-		frame.add(new Canvas() {
+		Canvas c = new Canvas() {
 			public void paint(Graphics g) {
 				super.paint(g);
 				g.drawImage(img, 0, 0, frame.getWidth(), frame.getHeight(), null);
 			}
-		}, BorderLayout.CENTER);
+		};
+		Dimension d = new Dimension(WIDTH, HEIGHT);
+		c.setPreferredSize(d);
+		c.setMinimumSize(d);
+		c.setMaximumSize(d);
+		
+		frame.add(c, BorderLayout.CENTER);
 		
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
 	
-	public static BufferedImage createImage() {
-		Integer[] colors = new Integer[NUMCOLORS*NUMCOLORS*NUMCOLORS];
-		int[] pixels = new int[WIDTH*HEIGHT];
+	public static BufferedImage createImage(PixelDiff pDiff, int width, int height, int nColors, int startX, int startY) {
+		Integer[] colors = new Integer[nColors*nColors*nColors];
+		int[] pixels = new int[width*height];
 		assert pixels.length == colors.length;
 		
 		// Randomize the color order
 		int q = 0;
-		for(int i=0; i < NUMCOLORS; i++) {
-			for(int j=0; j < NUMCOLORS; j++) {
-				for(int k=0; k < NUMCOLORS; k++) {
-					int r = i*255/(NUMCOLORS-1);
-					int g = j*255/(NUMCOLORS-1);
-					int b = k*255/(NUMCOLORS-1);
+		for(int i=0; i < nColors; i++) {
+			for(int j=0; j < nColors; j++) {
+				for(int k=0; k < nColors; k++) {
+					int r = i*255/(nColors-1);
+					int g = j*255/(nColors-1);
+					int b = k*255/(nColors-1);
 					colors[q++] = 0xFF000000 | r << 16 | g << 8 | b << 0;
 				}
 			}
@@ -84,94 +86,41 @@ public class Colors {
 			}
 		});*/
 		
-		// Constantly changing list of available coordinates (empty pixels which have non-empty neighbors)
 		HashSet<Vec2D> available = new HashSet<>();
 		
-		// Loop through all the colors that we want to place
-		int progressCounter = colors.length / 16;
-		for(int i = 0; i < colors.length; i++) {
-			int c = colors[i];
-			
-			Vec2D bestPos = null;
-			if(available.isEmpty()) {
-				// Use the starting posision
-				bestPos = new Vec2D(STARTX, STARTY);
-			} else {
-				// Find the best place from the list of available coordinates
-				// bestxy = available.AsParallel().OrderBy(xy => calcdiff(pixels, xy, colors[i])).First();
-				int bestVal = Integer.MAX_VALUE;
-				for(Vec2D pos : available) {
-					int diff = calcdiff(pixels, pos, c);
-					if(diff < bestVal) {
-						bestVal = diff;
-						bestPos = pos;
-					}
-				}
-				assert bestVal != Integer.MAX_VALUE;
+		Vec2D start = new Vec2D(startX, startY);
+		pixels[start.y*width + start.x] = colors[0];
+		
+		for(Vec2D n : start.getNeighbors(width, height)) {
+			if(pixels[n.y*width + n.x] == 0x00000000) {
+				available.add(n);
 			}
+		}
+		
+		for(int i = 1; i < colors.length; i++) {
+			int color = colors[i];
+			
+			Vec2D best = pDiff.calcPos(pixels, width, height, available, color);
+			assert best != null;
 			
 			// Put the pixel where it belong
-			assert pixels[bestPos.y*WIDTH + bestPos.x] == 0x00000000;
-			pixels[bestPos.y*WIDTH + bestPos.x] = c;
+			assert pixels[best.y*width + best.x] == 0x00000000;
+			pixels[best.y*width + best.x] = color;
 			
 			// Adjust the available list
-			available.remove(bestPos);
-			for(Vec2D nPos : bestPos.getNeighbors(WIDTH, HEIGHT)) {
-				if(pixels[nPos.y*WIDTH + nPos.x] == 0x00000000) {
-					available.add(nPos);
+			available.remove(best);
+			for(Vec2D n : best.getNeighbors(width, height)) {
+				if(pixels[n.y*width + n.x] == 0x00000000) {
+					available.add(n);
 				}
 			}
-			
-			if((i+1) == progressCounter) {
-				System.out.println("Status: " + (progressCounter / (float)colors.length));
-				progressCounter += colors.length / 16;
-			}
-			
 		}
 		assert available.size() == 0;
 		
-		BufferedImage img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
-		img.setRGB(0, 0, WIDTH, HEIGHT, pixels, 0, WIDTH);
+		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		img.setRGB(0, 0, width, height, pixels, 0, width);
 		
 		return img;
-	}
-	
-	public static int average(List<Integer> l) {
-		if(l.isEmpty()) throw new IllegalArgumentException("Cannot calculate the average of an empty list");
-		
-		int val=0;
-		for(int i : l) {
-			val += i;
-		}
-		return val / l.size();
-	}
-	
-	public static int min(List<Integer> l) {
-		if(l.isEmpty()) throw new IllegalArgumentException("Cannot find the minimum element in an empty list");
-		
-		int min = l.get(0);
-		for(int i=1; i<l.size(); i++) {
-			if(min < l.get(i)) {
-				min = l.get(i);
-			}
-		}
-		return min;
-	}
-	
-	public static int calcdiff(int[] pixels, Vec2D p, int c) {
-		List<Integer> diffs = new ArrayList<>(8);
-		for(Vec2D np : p.getNeighbors(WIDTH, HEIGHT)) {
-			int nc = pixels[np.y*WIDTH+np.x];
-			if(nc != 0x00000000) {
-				diffs.add(PixelDifference.coldiff(nc, c));
-			}
-		}
-		
-		if(AVERAGE) {
-			return average(diffs);
-		} else {
-			return min(diffs);
-		}
 	}
 
 }
---
+++ src/PixelDiff.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+
+public abstract class PixelDiff {
+	
+	public final static int colorDifference(int c1, int c2) {
+		int r = ((c1 & 0x00FF0000) >> 16) - ((c2 & 0x00FF0000) >> 16); 
+		int g = ((c1 & 0x0000FF00) >> 8) - ((c2 & 0x0000FF00) >> 8);
+		int b = ((c1 & 0x000000FF) >> 0) - ((c2 & 0x000000FF) >> 0);
+		return r*r + g*g + b*b;
+	}
+	
+	public final static List<Integer> colorDifferences(int[] pixels, int width, int height, Vec2D p, int color) {
+		List<Integer> diffs = new ArrayList<>(8);
+		
+		for(Vec2D np : p.getNeighbors(width, height)) {
+			int nc = pixels[np.y*width + np.x];
+			
+			if(nc != 0x00000000) {
+				diffs.add(colorDifference(nc, color));
+			}
+		}
+		
+		return diffs;
+	}
+	
+	public abstract Vec2D calcPos(int[] pixels, int width, int height, HashSet<Vec2D> available, int color);
+
+}
